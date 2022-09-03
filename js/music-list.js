fdd773fbb0c45329308420e631d64f52c880b495
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-03 12:48:04 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: " What Makes You Beautiful ",
+    artist: "One Direction",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Night Changes",
+    artist: "One Direction",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Perfect",
+    artist: "One Direction",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Best Song Ever",
+    artist: "One Direction",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Drag Me Down",
+    artist: "One Direction",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "History",
+    artist: "One Direction",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Steal My Girl",
+    artist: "One Direction",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "You & I",
+    artist: "One Direction",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Midnight Memories",
+    artist: "One Direction",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "One Thing",
+    artist: "One Direction",
+    img: "music-10",
+    src: "music-10"
+  },
+];
